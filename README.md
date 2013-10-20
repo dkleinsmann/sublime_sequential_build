440c7b4ed6db3e3158f840bb3b7776372373d1f6
Dan Kleinsmann <dkleinsmann@gmail.com>
2013-10-20 14:31:26 +0100

    Support for Sublime Text 3.  Allow _some_ build system variables to be used in build steps.

--- README.md
+++ README.md
@@ -1,45 +1,54 @@
 Sublime Sequential Build System
 ===============================
 
-This plugin serves the purpose of allowing multiple 'cmd' items in a build
-system.
+This plugin allows build system with multiple 'cmd' steps that will get
+executed sequentially.
 
 To use the plugin set the "target" attribute to "sequential_builder" and
 insert a list of build steps.
 
-It is required that the working_dir path be set as an absolute path.
-If working_dir is left blank. The directory of the currently open file will be
-used.
+Note: It is required that the working_dir path be set as an absolute path.
+For this reason it is recommended that one of the following variables are used.
+```
+$file_path
+$project_path
+```
 
-Warning: If working_dir is specified as a relative path, it is relative to 
-the location of the installed plugin.
+Note: Some build system variables cannot be used in the individual build steps.
+The allowed variables are listed below.
+```
+$file_path
+$file
+$file_name
+$file_extension
+$file_base_name
+$packages
+```
 
-Warning: The build system variables (ie $project_path) cannot be used in 
-individual build steps. The reason for this is that they are resolved outside
-of the python plugin framework.
+Warning: Snippets and default values for variables are not yet supported.
 
 An example build system using multiple build steps:
 ```JSON
 "build_systems":
 [
-	{
-	    "name": "Sequential Build Example (make)",
-	    "target": "sequential_builder",
-	    "working_dir": "${project_path}/build/",
-	    "build_sequence":
-	    [
-	        {
-	            "cmd": ["make", "build"]
-	            "file_regex": "^\\s*(.*?)\\((\\d+)\\):\\s+.*$"
-	        },
-	        {
-	            "cmd": ["make", "install"]
-	        },
-	        {
-	            "working_dir": "bin",
-	            "cmd": ["./main", "--id", "1"],
-	        }
-	    ]
-	}
+    {
+        "name": "Sequential Build Example (make)",
+        "target": "sequential_builder",
+        "working_dir": "${project_path}/build/",
+        "build_sequence":
+        [
+            {
+                "cmd": ["make", "${file}"]
+                "file_regex": "^\\s*(.*?)\\((\\d+)\\):\\s+.*$"
+            },
+            {
+                "cmd": ["make", "install"]
+            },
+            {
+                "working_dir": "../bin",
+                "cmd": ["$file_base_name", "--id", "1"],
+            }
+        ]
+    }
 ]
 ```
--- sequential_builder.py
+++ sequential_builder.py
@@ -2,21 +2,43 @@ import sublime
 import sublime_plugin
 import functools
 import copy
+import re
+import os
 
-# Import the ExecCommand Class used as a base class for the notifying execution
-#  class
-import sys, os
-sys.path.append(os.path.join(sublime.packages_path(), 'Default'))
-ExecCommand = getattr( __import__("exec", fromlist="ExecCommand"),
-                       'ExecCommand' )
-sys.path.remove(os.path.join(sublime.packages_path(), 'Default'))
+# If we're in Sublime Text 3, we can load the default module quite easily.
+if 3000 <= int(sublime.version()):
+    exec_mod = getattr( __import__( "Default" ), "exec" )
+
+# In older versions, the loading default module needs some trickery
+else:
+    import sys
+
+    # Check if the path to Default plugins has been loaded
+    seq_bld_default_imported = False
+    seq_bld_default_path = os.path.join(sublime.packages_path(), 'Default')
+    seq_bld_default_path = os.path.abspath(seq_bld_default_path)
+    for path in sys.path:
+        if os.path.abspath(path) == seq_bld_default_path:
+            seq_bld_default_imported = True
+
+    # If the path is not yet load it, we temporarily load it so we can do import
+    #  ExecCommand class from it.
+    if not seq_bld_default_imported:
+        sys.path.append(os.path.join(sublime.packages_path(), 'Default'))
+
+    # Import the ExecCommand
+    exec_mod = __import__( "exec" )
+
+    # Only remove the path if it wasn't there to begin with
+    if not seq_bld_default_imported:
+        sys.path.remove(os.path.join(sublime.packages_path(), 'Default'))
 # Import done
 
 # The ExecCommand class is used for running build commands using Popen
 #
 # By overloading the on_finished() method, we can trigger a callback when
 #  a build completes.
-class NotifyingExecCommand(ExecCommand):
+class NotifyingExecCommand(exec_mod.ExecCommand):
 
     # Store the callback in the class to avoid name clashes.
     callback = None
@@ -30,7 +52,7 @@ class NotifyingExecCommand(ExecCommand):
         exit_code = proc.exit_code()
 
         # Do the superclass on_finished() first
-        ExecCommand.on_finished( self, proc )
+        exec_mod.ExecCommand.on_finished( self, proc )
 
         # Only start the next build step if this one was successful
         if exit_code == 0 or exit_code == None:
@@ -42,6 +64,10 @@ class NotifyingExecCommand(ExecCommand):
 # This is the Command class called when a build system sets the
 #  target to "sequential_builder".
 class SequentialBuilderCommand(sublime_plugin.WindowCommand):
+
+    # This is the regex used to extract build system variables from
+    #  arguments passed to this plugin.
+    variable_regex = re.compile( r'(\$[{]?)(\w+)([}]?)' )
 
     # The actual execution method called when the build system begins work
     def run(self, **args):
@@ -55,6 +81,29 @@ class SequentialBuilderCommand(sublime_plugin.WindowCommand):
             # Note down the list of steps, we'll iterate over that list
             #  later.
             self.build_sequence = build_sequence
+
+            # Create all the build system variables so they can be used
+            #  in individual build steps
+            op = os.path
+            var = dict()
+            var['packages'] = op.abspath( sublime.packages_path() )
+
+            var['file'] = op.abspath( self.window.active_view().file_name() )
+            var['file_path'] = op.dirname( var['file'] )
+            var['file_name'] = op.basename( var['file'] )
+            base, ext = op.splitext( var['file_name'] )
+            var['file_extention'] = ext
+            var['file_base_name'] = base
+
+            # <TODO>: Find out how to get current project path.
+            #var['project'] = op.abspath(".")
+            #var['project_path'] = op.dirname( var['project'] )
+            #var['project_name'] = op.basename( var['project'] )
+            #base, ext = op.splitext( var['project_name'] )
+            #var['project_extention'] = ext
+            #var['project_base_name'] = base
+
+            self.variables = var
 
             # The remaining items in args are the other attributes of the
             #  build system, described in the top level of it's hierarchy.
@@ -76,6 +125,56 @@ class SequentialBuilderCommand(sublime_plugin.WindowCommand):
                                    " of 'cmd's. Please check your build "
                                    " system for syntax errors." )
 
+    def _replace_var_str( self, string ):
+
+        # Go through matches in descending order of size. This
+        #  stops $file_path being written to as the $file variable
+        matches = self.variable_regex.findall( string )
+        matches = sorted( matches,
+            key=lambda x: len(x[1]),
+            reverse=True )
+
+        # <TODO> add default values: (${file_name:default_value})
+        # <TODO> file ${file_name/.php/.txt} to replace txt for php
+        new_string = string
+        for lhs, var, rhs in matches:
+            if self.variables.has_key( var ):
+                new_string = new_string.replace(
+                    "%s%s%s" % (lhs, var, rhs),
+                    self.variables[var] )
+
+        return new_string
+
+    def _replace_var_list( self, the_list ):
+        new_list = list()
+        for item in the_list:
+            if type(item) is unicode:
+                value = self._replace_var_str( item )
+            elif type(item) is list:
+                value = self._replace_vars_list( item )
+            elif type(item) is dict:
+                value = self._replace_vars_dict( item )
+            new_list.append( value )
+        return new_list
+
+    def _replace_var_dict( self, the_dict ):
+
+        # If the user has tried to use build system variables we try
+        #  and populate them.
+        for key, value in the_dict.items():
+
+            # Replace user variables in values that are strings
+            if type(value) is unicode and "regex" not in key:
+                the_dict[key] = self._replace_var_str( value )
+
+            elif type(value) is list:
+                the_dict[key] = self._replace_var_list( value )
+
+            elif type(value) is dict:
+                the_dict[key] = self._replace_var_dict( value )
+
+        return the_dict
+
     # Each item in the build sequence corresponds to a call to build_step()
     def build_step( self ):
 
@@ -92,32 +191,23 @@ class SequentialBuilderCommand(sublime_plugin.WindowCommand):
             # Get the step specific options
             step_args = self.build_sequence[index]
 
-            # And double check that the user hasn't tried to use
-            # ${project_path} or any other variables
-            for key, value in step_args.items():
-                if type(value) is unicode and \
-                   "regex" not in key         \
-                   and "$" in value:
-                    sublime.error_message( "Error: Sequential Builder cannot "
-                        "use variables." )
-                    return
+            # And try and populate the build system variables
+            step_args = self._replace_var_dict( step_args )
 
-            # If the build step has a working_dir argument, we may have to
-            #  do a little more work
-            if ( step_args.has_key( 'working_dir' ) ):
-
-                # We relative paths as relative to the top level working_dir
-                #  path.
-                if not os.path.isabs( step_args['working_dir'] ):
-                    step_args['working_dir'] = os.path.join(
-                        args['working_dir'], step_args['working_dir'] )
+            # Relative paths are relative to the top level working_dir
+            #  path.
+            if step_args.has_key( 'working_dir' ) and \
+               not os.path.isabs( step_args['working_dir'] ):
+                step_args['working_dir'] = os.path.join(
+                    args['working_dir'],
+                    step_args['working_dir'] )
 
             # Here we update any step specific options
             args.update( step_args )
             args['working_dir'] = os.path.abspath(args['working_dir'])
 
             # Run the command
-            print "Build Step (%d): %s" % (index+1, args)
+            print( "Build Step (%d): %s" % (index+1, args) )
             try:
                 self.window.run_command("notifying_exec", args)
             except:
